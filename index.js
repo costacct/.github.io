e2c0fa6dc9f6916a8674015895bd3b86dcd65d8a
Miaoqin Wu <ga92fes@tum.de>
2020-12-17 18:47:37 +0100

    new version multiple times dragable

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
-
-
 $(function() {
-  let currentTag = ''
-  let isFixedSort = false
+	let currentTag = ""
+	let isFixedSort = false
 
-  /* 被拖拽的元素 */
-	$(".drag").on("dragstart", function(ev) {
+	/* 被拖拽的元素 */
+	$(".drag").on("dragstart",function (ev) {
+		console.log(ev, "拖动")
+    console.log(this, "拖动")
+    $(this).children().remove()
 		ev.originalEvent.dataTransfer.setData(
 			"val",
 			JSON.stringify({
-				htmlString: $(this).html(),
-        dataIndex: $(this).attr("data-index")
-      })
-    )
-    currentTag = ev.target
+				htmlString: $(ev.target).html(),
+				dataIndex: $(ev.target).attr("data-index")
+			})
+		)
+		currentTag = ev.target
 	})
 
 	$(".wrapper").on("dragover", function(ev) {
@@ -22,47 +23,46 @@ $(function() {
 	})
 	/* 拖动结束事件 */
 	$(".drag").on("dragend", function(ev) {
+		ev.preventDefault()
 		console.log("拖拽结束")
 	})
 
 	/* 拖拽容器 */
 	$(".wrapper").on("drop", function(ev) {
-    ev.preventDefault()
-    let selfIndex = $(this).attr('data-index')
-		console.log("执行放置事件 也就是数据的交互")
-    let data = JSON.parse(ev.originalEvent.dataTransfer.getData("val"))
-    let oli = ''
-    if(data.dataIndex == selfIndex) {
-      isFixedSort = true
-      oli =  $(`<li>${data["htmlString"]}  
-        <span class = "right"> TRUE </span>
-      </li>`)
-    } else{
-     oli = $(`<li>${data["htmlString"]}  
-     <span class = 'wrong'> FALSE </span>
-     </li>`)
-      isFixedSort = false
-    }
-    $(this).append($(oli))
-    
-    /* 删除掉原来的元素 */
+		ev.preventDefault()
+		console.log(ev)
+		// return false
+		let selfIndex = $(this).attr("data-index")
+		console.log(ev.originalEvent.dataTransfer.getData("val"), "测试")
+		let data = JSON.parse(ev.originalEvent.dataTransfer.getData("val"))
+		let oli = ""
+
+    data.dataIndex == selfIndex ? isFixedSort = true : isFixedSort = false
+
+  	oli = $(`<p class="drag" draggable= ${!isFixedSort ? 'true' : 'false'} data-index = ${data.dataIndex}>${data["htmlString"]}  
+     <span class = ${isFixedSort ? 'right' : 'wrong'}>  ${isFixedSort ? 'TRUE' : 'FALSE'} </span>
+     </p>`)
+		$(oli).on("dragstart", function(ev)  {
+      $(this).children().remove()
+			ev.originalEvent.dataTransfer.setData(
+				"val",
+				JSON.stringify({
+					htmlString: $(ev.target).html(),
+					dataIndex: $(ev.target).attr("data-index")
+				})
+			)
+      currentTag = ev.target
+      console.log(currentTag, '12');
+		})
+		$(this).append($(oli))
+
+		/* 删除掉原来的元素 */
     $(currentTag).remove()
-   
-    
+    $(currentTag).children().remove()
 	})
 	$(".wrapper").on("dragenter", function(ev) {
-		console.log(this)
+		// console.log(this)
 		ev.preventDefault()
-    console.log("拖动进入容器事件", "dragenter")
-    
-  })
-  
-  $('.btn').on('click',function(){
-    if(isFixedSort) {
-      alert('完成')
-    }else{
-      alert('失败')
-    }
-  })
+		console.log("拖动进入容器事件", "dragenter")
+	})
 })
-
