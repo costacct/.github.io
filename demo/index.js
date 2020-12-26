527cd050018171fa81a6c9c822661bcb47827a12
lesson <lisen15655>
2020-12-26 22:50:41 +0800

    备注解释

--- demo/index.js
+++ demo/index.js
@@ -4,7 +4,6 @@ $(function() {
 
   /* elements to be dragged into income statement*/
   $(".drag").on("dragstart",function (ev) {
-    //console.log(ev.target, 'target')
     let isBold = $(ev.target).hasClass('bold')
     $(this).children().remove()
     ev.originalEvent.dataTransfer.setData(
@@ -19,7 +18,7 @@ $(function() {
   })
 
   $(".container").on("dragover", function(ev) {
-    ev.preventDefault()
+    ev.preventDefault() // 清除默认事件
   })
   /* drag event ends */
   $(".drag").on("dragend", function(ev) {
@@ -30,8 +29,7 @@ $(function() {
   $(".container").on("drop", function(ev) {
     ev.preventDefault()
     let selfIndex = $(this).attr("data-index")
-    console.log(ev.originalEvent.dataTransfer.getData("val"), "test")
-    let data = JSON.parse(ev.originalEvent.dataTransfer.getData("val"))
+    let data = JSON.parse(ev.originalEvent.dataTransfer.getData("val"))  // 序列化解析
     let oli = ""
 
     data.dataIndex == selfIndex ? isFixedSort = true : isFixedSort = false
@@ -39,27 +37,26 @@ $(function() {
       ${data["htmlString"]}
      <span class = ${isFixedSort ? 'true' : 'wrong'}>  ${isFixedSort ? 'TRUE' : 'FALSE'} </span>
      </li>`)
+    //  动态追加的元素 追加的事件 需要采用事件委托 或者 重新 执行事件
     $(oli).on("dragstart", function(ev)  {
+      let isBold = $(ev.target).hasClass('bold')
       $(this).children('span').remove()
       ev.originalEvent.dataTransfer.setData(
         "val",
         JSON.stringify({
           htmlString: $(ev.target).html(),
+          isBold:isBold,
           dataIndex: $(ev.target).attr("data-index")
         })
       )
       currentTag = ev.target
-      console.log(currentTag, '12');
     })
+    // 放到格子里
     $(this).append($(oli))
 
     /* delete original elements */
     $(currentTag).remove()
     $(currentTag).children().remove()
   })
-  $(".container").on("dragenter", function(ev) {
-    ev.preventDefault()
-    return false
 
-  })
 })
--- index.js
+++ index.js
@@ -4,8 +4,6 @@ $(function() {
 
 	/* 被拖拽的元素 */
 	$(".drag").on("dragstart",function (ev) {
-		console.log(ev, "拖动")
-    console.log(this, "拖动")
     $(this).children().remove()
 		ev.originalEvent.dataTransfer.setData(
 			"val",
@@ -19,21 +17,16 @@ $(function() {
 
 	$(".wrapper").on("dragover", function(ev) {
 		ev.preventDefault()
-		console.log("悬浮容器上方事件")
 	})
 	/* 拖动结束事件 */
 	$(".drag").on("dragend", function(ev) {
 		ev.preventDefault()
-		console.log("拖拽结束")
 	})
 
 	/* 拖拽容器 */
 	$(".wrapper").on("drop", function(ev) {
 		ev.preventDefault()
-		// console.log(ev)
-		// return false
 		let selfIndex = $(this).attr("data-index")
-		console.log(ev.originalEvent.dataTransfer.getData("val"), "测试")
 		let data = JSON.parse(ev.originalEvent.dataTransfer.getData("val"))
 		let oli = ""
 
